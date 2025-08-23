493f2da1c67c5dafbeee175dde5ca27e008ef5e0
Soham Sareriya <sohamsareriya@gmail.com>
2025-08-24 03:54:50 +0530

    changes done

--- src/main/java/com/quickpoll/backend/config/SecurityConfig.java
+++ src/main/java/com/quickpoll/backend/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-                Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+                Arrays.asList("https://quickpoll-frontend-ten.vercel.app/", "http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH",
                 "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
